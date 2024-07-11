8402fa042914c9215ab61e31a0df0e0848ea411c
HX <heikosohn@gmail.com>
2024-07-11 15:25:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,19 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  
+  // Get info from request
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+
+
+  res.json({
+    "ipaddress": ipaddress,
+    "language": language,
+    "software": userAgent
+  });
 });
 
 // listen for requests :)
